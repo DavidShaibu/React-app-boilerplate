114243537c2ae4a3592b116512d3c0c5d01b2ded
DavidShaibu <davidshaibu44@gmail.com>
2023-05-09 05:57:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+ 
+function App() {
+  const color = "danger";
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>MyAlert</Alert> }
+      <Button color={color} onClick={() => setAlertVisibility(true)}>
+        MyButton
+      </Button>
+    </div>
+    );
+}
+
+
+export default App;
