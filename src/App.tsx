3c3badf73a45ff570420be058d726a196f0c4363
DavidShaibu <davidshaibu44@gmail.com>
2023-05-13 14:57:31 +0100

    expandable text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import Button from "./components/Button/Button";
-import NavBar from "./components/NavBar/NavBar";
-import CartItem from "./components/CartItem/CartItem";
+import ExpandableText from "./components/ExpandableText/ExpandableText";
 
  
 function App() {
-const [cartItem, setCartItem] = useState(["Product 1", "Product 2", "Product 3", "Product 4"]);
+  const [cartItem, setCartItem] = useState(["Product 1", "Product 2", "Product 3", "Product 4"]);
 
-const handleClick = () => {
+  const handleClick = () => {
   setCartItem([]);
-};
-
+  };
 
   return (
-    <div>
-      <NavBar cartItem={cartItem.length}/>
-      <CartItem cartItem={cartItem} onClear={handleClick}/>
-    </div>
+    <>
+    <ExpandableText maxChars={10}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque reiciendis voluptatibus laborum saepe nulla, perferendis quibusdam porro eos nostrum vitae odio eius vel ipsa amet dolorem quis animi, et temporibus sequi. Cupiditate assumenda nesciunt maxime amet ipsam quam aspernatur sunt excepturi, accusamus nam at numquam aliquid asperiores recusandae vitae odit repellat quos quod saepe! Iure porro officiis possimus? Ab asperiores, soluta rerum laboriosam dignissimos minima? Deserunt dolorum tenetur repudiandae aspernatur magnam earum vel consectetur odio officia. Magni voluptatum totam et neque dignissimos repellendus maiores, fugit error laudantium quasi amet eligendi in sunt earum praesentium deserunt ipsa ratione voluptate ex autem.
+    </ExpandableText>
+    </>
     );
+
+
+
 }
 
 export default App;
