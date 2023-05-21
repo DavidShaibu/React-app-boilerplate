0675989b00b0a48ae09b8c5aa822dab193b5794f
DavidShaibu <davidshaibu44@gmail.com>
2023-05-21 04:45:02 +0100

    form hook

--- src/Form/form.tsx
+++ src/Form/form.tsx
@@ -1,42 +1,39 @@
 import { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form"
+
+interface FormData{
+    item: string;
+    amount: number;
+    category: string;
+}
 
 const form = () => {
 
-  const [person, setPerson] = useState({
-    item: "",
-    amount: 0,
-  });
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+  console.log(errors)
+  const onSubmit = (data: FieldValues) => {console.log(data)};
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
-        <label htmlFor="description" className="form-label">
+        <label htmlFor="item" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, item: event.target.value })
-          }
-          value={person.item}
-          id="description"
+          {...register("item", {required: true, minLength:3})}  
+          id="item"
           type="text"
           className="form-control"
         />
+        {errors.item?.type === "required" && <p className="text-danger">The name field is required</p>}
+        {errors.item?.type === "minLength" && <p className="text-danger">The must be at least 3 characters</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, amount: parseInt(event.target.value) })
-          }
-          value={person.amount}
+          {...register("amount")}
           id="amount"
           type="number"
           className="form-control"
