7cd877afdf64497f8ed3a21740339a7dce801da5
DavidShaibu <davidshaibu44@gmail.com>
2023-05-21 04:06:05 +0100

    state hook to access input value

--- src/Form/form.tsx
+++ src/Form/form.tsx
@@ -1,40 +1,56 @@
-import { useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 const form = () => {
-  
-  const nameRef = useRef<HTMLInputElement>(null);  
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = {name: "", age: 0};
-    
+
+  const [person, setPerson] = useState({
+    item: "",
+    amount: 0,
+  });
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
+
   return (
-    <form onSubmit={(event) => {
-        event.preventDefault();
-
-        if (nameRef.current != null)
-        person.name = nameRef.current.value;
-
-        if(ageRef.current != null)
-        person.age = parseInt(ageRef.current.value);
-
-        console.log(person);
-        }}>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="description" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, item: event.target.value })
+          }
+          value={person.item}
+          id="description"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input ref={ageRef} id="amount" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, amount: parseInt(event.target.value) })
+          }
+          value={person.amount}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="category" className="form-label">Category</label>
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
         <input id="category" type="text" className="form-control" />
       </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 };
