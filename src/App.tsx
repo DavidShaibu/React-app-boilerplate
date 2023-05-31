882b5b8b20b1a59fc59d5c9b8643ee774d5835ca
DavidShaibu <davidshaibu44@gmail.com>
2023-05-31 18:19:00 +0100

    http requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from "react";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import categories from "./expense-tracker/categories";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    {id:1, description: "aaa", amount:10, category: "Utilities"},
-    {id:2, description: "bbb", amount:10, category: "Utilities"},
-    {id:3, description: "ccc", amount:10, category: "Utilities"},
-    {id:4, description: "ddd", amount:10, category: "Utilities"},
-  ]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
-  if (expenses.length === 0) return null;
-
-  const visibleExpenses = selectedCategory ? expenses.filter(expense => expense.category === selectedCategory) : expenses;
+  // afterRender
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch( err => setError(err.message))
+  }, []);
 
   return (
     <>
-      <div className="mb-3">
-        <ExpenseForm onSubmit={(expense) => setExpenses([...expenses, {...expense, id: expenses.length + 1}])}/>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)}/>
-      </div>
-      <ExpenseList expenses={visibleExpenses} onDelete={(id) => setExpenses(expenses.filter(expense => expense.id !== id))}/>
+      { error && <p className="text-danger"> {error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
-    );
-
+  );
 }
 
 export default App;
-
